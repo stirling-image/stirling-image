c2130148c480ac1847c9ad1a8698201394a4204d
ashim-hq <ashim.hq@gmail.com>
2026-04-22 19:15:38 +0800

    fix: add statement-breakpoint separators to analytics migration

--- apps/api/drizzle/0009_analytics_consent.sql
+++ apps/api/drizzle/0009_analytics_consent.sql
@@ -1,3 +1,5 @@
 ALTER TABLE users ADD COLUMN analytics_enabled integer;
+--> statement-breakpoint
 ALTER TABLE users ADD COLUMN analytics_consent_shown_at integer;
+--> statement-breakpoint
 ALTER TABLE users ADD COLUMN analytics_consent_remind_at integer;
