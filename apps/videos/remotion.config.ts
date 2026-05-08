ddcead66fb15f8381436ace22e2c145b4f6c13a2
SnapOtter <snapotter.hq@gmail.com>
2026-05-08 16:06:46 +0800

    feat(videos): scaffold Remotion workspace with React 18 and Tailwind v3

---
+++ apps/videos/remotion.config.ts
@@ -0,0 +1,6 @@
+import { Config } from "@remotion/cli/config";
+import { enableTailwind } from "@remotion/tailwind";
+
+Config.setVideoImageFormat("jpeg");
+Config.setOverwriteOutput(true);
+Config.overrideWebpackConfig((config) => enableTailwind(config));
---
+++ apps/videos/src/Root.tsx
@@ -0,0 +1,6 @@
+import "./style.css";
+import { Composition } from "remotion";
+
+export const RemotionRoot: React.FC = () => {
+  return <>{/* Compositions will be added as they are built */}</>;
+};
